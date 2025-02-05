ceca02f806aacadc76738d30354a04d92bb83bfb
Jerry Ogwa <jerifortune@gmail.com>
2025-02-05 12:18:30 -0500

    renaming columns

--- kaggle_data_extraction.py
+++ kaggle_data_extraction.py
@@ -16,12 +16,37 @@ def read_file(file_path):
      else:
          raise ValueError("Invalid file type. This function only acecepts json and csv file formats")
          
+df = read_file('CloudWatch_Traffic_Web_Attack.csv')
 
-df = read_file("CloudWatch_Traffic_Web_Attack.csv")
 
 
 # Converting to dataframe
 
 df = pd.DataFrame(df)
 
+df.head(5)
+df.columns
+
+
+# Renaming the columns
+
+cols_new_name_dict = {
+    'bytes_in':'', 
+    'bytes_out':'', 
+    'creation_time':'creation_time', 
+    'end_time':'nd_time', 
+    'src_ip':'source_ip',
+    'src_ip_country_code':'source_ip_country_code', 
+    'protocol':'protocol' ,
+    'response.code':'response_code', 
+    'dst_port':'destination_port',
+    'dst_ip':'destination_ip', 
+    'rule_names':'rule', 
+    'observation_name':'observation', 
+    'source.meta':'source_metadata',
+    'source.name':'source_name', 
+    'time':'event_detection_time', 
+    'detection_types':'detection_types'
+    }
+df.rename(cols_new_name_dict, axis=1, inplace=True)
 df.head(5)
