5761f49694ba8d8498493e06b65919cb36f8ccbd
Jerry Ogwa <jerifortune@gmail.com>
2025-02-05 11:43:04 -0500

    adding a function  to read json and csv files

--- kaggle_data_extraction.py
+++ kaggle_data_extraction.py
@@ -1,6 +1,27 @@
-import kagglehub
+import kagglehub;
+import pandas as pd;
 
-# Download latest version
+
+# Download  kaggle cybersecurity-suspicious-web-threat-interactions dataset
 path = kagglehub.dataset_download("jancsg/cybersecurity-suspicious-web-threat-interactions")
 
 print("Path to dataset files:", path)
+
+# use pandas  to read the dataset - json and csv format only
+def read_file(file_path):
+     if file_path.endswith('.json'):
+         return pd.read_csv(file_path)
+     elif file_path.endswith('.csv'):
+         return pd.read_csv(file_path)
+     else:
+         raise ValueError("Invalid file type. This function only acecepts json and csv file formats")
+         
+
+df = read_file("/Users/jerifortune/Documents/kaggle-suspicious-web-threat/dataset/CloudWatch_Traffic_Web_Attack.csv")
+
+
+# Converting to dataframe
+
+df = pd.DataFrame(df)
+
+df.head(5)
